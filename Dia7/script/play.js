2d41bed02e81bae23ae2fc5b536cda6d61b53788
Sarasofia1214 <dasaro1206@gmail.com>
2025-04-24 23:11:28 -0500

    feat: :sparkles: Starting the script

---
+++ Dia7/script/play.js
@@ -0,0 +1,12 @@
+let higher = document.getElementById("buttom_box_red");
+let lower = document.getElementById("buttom_box_green");
+
+function showCard (){
+let xml = new XMLHttpRequest();
+let url = `https://deckofcardsapi.com/api/deck/gpv7jqqa44rj/draw/?count=10`
+xml.open(`GET, url`)
+let card = JSON.parse(this.cardText)
+}
+
+function card(){
+    if (data)['cards']['images']['png']}
