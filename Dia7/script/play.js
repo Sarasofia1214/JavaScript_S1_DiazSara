210ca31b4745c2c0937b7a31542e0d6428c61772
Sarasofia1214 <dasaro1206@gmail.com>
2025-04-27 21:55:13 -0500

    feat: :memo: Changin the logical code in the js file

--- Dia7/script/play.js
+++ Dia7/script/play.js
@@ -1,49 +1,102 @@
 let higher = document.getElementById("buttom_box_red");
 let lower = document.getElementById("buttom_box_green");
+let scoreButton = document.getElementById("score_button");
+let restartButton = document.getElementById("restart_button");
+
+let ID;
+let cartaActual;
+let cartaSiguiente;
+let puntos = 0;
+
+
+const backCardURL = "https://www.divulgamat.net/html/images/dorso5c.jpg";
 
 
 async function llamarAPI() {
     let response = await fetch("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1");
     let data = await response.json();
-    let deck_ID = data["deck_id"]
-    return deck_ID
+    return data["deck_id"];
 }
 
-async function mostrarCarta(ID){
+
+function convertirValor(valor) {
+    if (valor === "ACE") return 1;       
+    if (valor === "JACK") return 11;      
+    if (valor === "QUEEN") return 12;   
+    if (valor === "KING") return 13;   
+    return parseInt(valor);               
+}
+
+async function empezarJuego() {
+    ID = await llamarAPI();
+    
     let response = await fetch(`https://www.deckofcardsapi.com/api/deck/${ID}/draw/?count=2`);
     let data = await response.json();
-    let card = document.getElementById("card_yo")
-    card.src = data["cards"][0]["image"]
+    
+    cartaActual = data["cards"][0];
+    cartaSiguiente = data["cards"][1];
+    
+    document.getElementById("card_yo").src = cartaActual["image"];
+    document.getElementById("card_rest").src = backCardURL; 
+    
+    scoreButton.innerText = `Puntos: ${puntos}`;
 }
 
-async function mostrarCartas(ID){
-    let response = await fetch(`https://www.deckofcardsapi.com/api/deck/${ID}/draw/?count=2`);
-    let data = await response.json();
-    let cards = document.getElementById("card_rest")
-    cards.src = data["cards"][0]["image"]
+
+async function jugar(eleccion) {
+
+    document.getElementById("card_rest").src = cartaSiguiente["image"];
+    let valorActual = convertirValor(cartaActual["value"]); 
+    let valorSiguiente = convertirValor(cartaSiguiente["value"]); 
+    let acierto = false;
+
+    if (eleccion === "higher" && valorSiguiente < valorActual) {
+        acierto = true;
+    } else if (eleccion === "lower" && valorSiguiente > valorActual) {
+        acierto = true;
+    }
+
+    if (acierto) {
+        puntos++;
+        scoreButton.innerText = `Points: ${puntos}`;
+
+        cartaActual = cartaSiguiente;
+        document.getElementById("card_yo").src = cartaActual["image"];
+        let response = await fetch(`https://www.deckofcardsapi.com/api/deck/${ID}/draw/?count=1`);
+        let data = await response.json();
+        cartaSiguiente = data["cards"][0];
+        document.getElementById("card_rest").src = backCardURL; 
+
+    } else {
+        scoreButton.innerText = `You lose Total: ${puntos}`;
+
+        higher.disabled = true;
+        lower.disabled = true;
+    }
 }
 
-async function empezarJuego(){
-    let deckID = await llamarAPI()
-    mostrarCarta(deckID)
-    mostrarCartas(deckID)
+
+function reiniciarJuego() {
+    puntos = 0;
+    scoreButton.innerText = `Puntos: ${puntos}`;
+    higher.disabled = false;
+    lower.disabled = false;
+
+    empezarJuego();
 }
 
-empezarJuego(); 
+
+higher.addEventListener("click", () => {
+    jugar("higher");
+});
+
+lower.addEventListener("click", () => {
+    jugar("lower");
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+restartButton.addEventListener("click", reiniciarJuego);
+empezarJuego();
 
 
 //function showCard(ID) {
