a6e5984476484c5103c07720007738ecb032aba6
Sarasofia1214 <dasaro1206@gmail.com>
2025-05-02 00:23:46 -0500

    feat: :sparkles: Trying the put code

--- Dia12/js/script.js
+++ Dia12/js/script.js
@@ -1,17 +1,18 @@
 
+let selectop=document.getElementById("option");
+let edit=document.getElementById("Fromedit");
+
+
 function showArts() {
     axios.get('https://681401d6225ff1af1627ad7f.mockapi.io/Famous_WorksOfArts')
       .then(respuesta => {
         const Arts = respuesta.data;
-        let tbody = document.getElementById("table");
+        let content = document.getElementById("table");
   
         Arts.forEach(obra => {
-          tbody.innerHTML += `
-              <tr>
-                <td>${obra.Title}</td>
-                <td><img src="${obra.Picture}" alt="${obra.Title}"></td>
-              </tr>
-          `;
+          content.innerHTML += 
+          `<td>${obra.Title}</td>
+           <td><img src="${obra.Picture}" alt="${obra.Title}"></td>`;
         });
       })
       .catch(error => {
@@ -19,10 +20,16 @@ function showArts() {
       });
   }
   
-  function editArts(){
-    axios.put('https://681401d6225ff1af1627ad7f.mockapi.io/Famous_WorksOfArtshttps://681401d6225ff1af1627ad7f.mockapi.io/Famous_WorksOfArts/${id}')
-
-  }
+  function editArts(id, newData) {
+    axios.put(`https://681401d6225ff1af1627ad7f.mockapi.io/Famous_WorksOfArts/${id}`, newData)
+      .then(response => {
+        console.log('Obra editada:', response.data);
+        mostrarObras(); 
+      })
+      .catch(error => {
+        console.error('Error', error);
+      });
+  }  
 
   document.getElementById('form').addEventListener('submit', function(event) {
     event.preventDefault(); 
@@ -32,9 +39,21 @@ function showArts() {
   
     if (action === "get") {
       showArts();
-    };
-    elif (action === "put")
+    }
+    else if (action === "put"){
         editArts();
-    
+    }
   });
+document.querySelector('#Formedit').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+    let Id = document.getElementById("id").value;
+    let Newtitle =document.getElementById("title").value;
+    let Newpicture=document.getElementById("picture").value;
   
+    const newData = {
+      Title: Newtitle,
+      Picture:Newpicture
+    };
+  
+    editArts(Id, newData);
+  });
